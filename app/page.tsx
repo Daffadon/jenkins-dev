ddf36c0e988f8aa5b132aa4f39bd1525b3cb88f0
Daffadon <daffaputranarendra9@gmail.com>
2024-05-11 19:59:55 +0700

    made a change for triggering build

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,6 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
           <p>HI EVERYONE THIS IS TEST FOR BUILD TRIGGER </p>
-          <p>HI EVERYONE THIS IS TEST FOR BUILD TRIGGER#2 </p>
         </a>
       </div>
     </main>
